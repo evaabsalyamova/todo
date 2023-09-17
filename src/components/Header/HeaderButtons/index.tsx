a0191707e571952a10c5909d88999f70091400c4
evaabsalyamova <absalyamovaeva@gmail.com>
2023-09-17 12:29:05 +0400

    Change header button's color & add theme mode icons

--- src/components/Header/HeaderButtons/index.tsx
+++ src/components/Header/HeaderButtons/index.tsx
@@ -5,7 +5,7 @@ const HeaderButtons: React.FunctionComponent = () => {
   return (
     <div className="headerButtons">
       <button className="headerButton">{icons.settings}</button>
-      <button className="headerButton">{icons.settings}</button>
+      <button className="headerButton">{icons.lightMode}</button>
     </div>
   );
 };
