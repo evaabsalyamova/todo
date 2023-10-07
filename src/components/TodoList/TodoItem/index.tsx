11b00e8898916ff5b85dfa571da975ff211ae91e
evaabsalyamova <absalyamovaeva@gmail.com>
2023-10-07 14:31:17 +0400

    Add delete button click handling

--- src/components/TodoList/TodoItem/index.tsx
+++ src/components/TodoList/TodoItem/index.tsx
@@ -1,4 +1,6 @@
+import { sendDeleteTodo } from "../../../api/todoList";
 import { icons } from "../../../assets/icons";
+import { deleteTodo, useAppDispatch } from "../../../redux";
 import { ITodo } from "../../../types/todo";
 import "./styles.css";
 
@@ -7,12 +9,21 @@ interface IProps {
 }
 
 const TodoItem: React.FunctionComponent<IProps> = ({ todo }) => {
+  const dispatch = useAppDispatch();
+
+  const handleDeleteButtonClick = (): void => {
+    dispatch(deleteTodo({ id: todo.id }));
+    sendDeleteTodo({ id: todo.id });
+  };
+
   return (
     <div className="todoItem">
       <div className="textBlock">{todo.text}</div>
       <div className="todoItemButtons">
         <div className="todoItemButton">{icons.checkMark}</div>
-        <div className="todoItemButton">{icons.trash}</div>
+        <div className="todoItemButton" onClick={handleDeleteButtonClick}>
+          {icons.trash}
+        </div>
       </div>
     </div>
   );
