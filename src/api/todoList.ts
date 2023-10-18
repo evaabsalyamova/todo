35da6b84a67035c15392a411ec766d9bcafdd1dd
evaabsalyamova <absalyamovaeva@gmail.com>
2023-10-18 17:31:23 +0400

    Create `sendTodoUpdates` & `sendNewTodo`

--- src/api/todoList.ts
+++ src/api/todoList.ts
@@ -1,8 +1,10 @@
+import { IExternalTodo, ITodo } from "../types/todo";
+
 const headers = {
   "Content-type": "application/json; charset=UTF-8",
 };
 
-export const getTodos = async () => {
+export const getTodos = async (): Promise<IExternalTodo[]> => {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
     method: "GET",
     headers,
@@ -13,9 +15,40 @@ export const getTodos = async () => {
   return response;
 };
 
-export const sendDeleteTodo = ({ id }: { id: number }) => {
+export const sendDeleteTodo = ({ id }: { id: number }): void => {
   fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
     method: "DELETE",
+    headers,
+  });
+};
+
+export const sendTodoUpdates = ({ todo }: { todo: ITodo }): void => {
+  fetch(`https://jsonplaceholder.typicode.com/posts/${todo.id}`, {
+    method: "PUT",
+    body: JSON.stringify({
+      id: todo.id,
+      title: todo.title,
+      body: todo.text,
+      userId: 1,
+    }),
+    headers,
+  });
+};
+
+export const sendNewTodo = ({
+  title,
+  body,
+}: {
+  title: string;
+  body: string;
+}): void => {
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      body,
+      userId: 1,
+    }),
     headers,
   });
 };
