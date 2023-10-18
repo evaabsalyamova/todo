edf775b0aa1790d60ccdbd991d4baeb4e2e0e6bd
evaabsalyamova <absalyamovaeva@gmail.com>
2023-10-18 17:39:53 +0400

    Add update text todo feature

--- src/components/TodoList/TodoItem/index.tsx
+++ src/components/TodoList/TodoItem/index.tsx
@@ -1,6 +1,12 @@
-import { sendDeleteTodo } from "../../../api/todoList";
+import { useState } from "react";
+import { sendDeleteTodo, sendTodoUpdates } from "../../../api/todoList";
 import { icons } from "../../../assets/icons";
-import { deleteTodo, toggleTodoReady, useAppDispatch } from "../../../redux";
+import {
+  changeTextTodo,
+  deleteTodo,
+  toggleTodoReady,
+  useAppDispatch,
+} from "../../../redux";
 import { ITodo } from "../../../types/todo";
 import "./styles.css";
 
@@ -9,6 +15,9 @@ interface IProps {
 }
 
 const TodoItem: React.FunctionComponent<IProps> = ({ todo }) => {
+  const [newText, setNewText] = useState<string>(todo.text);
+  const [isEditMode, setIsEditMode] = useState<boolean>(false);
+
   const dispatch = useAppDispatch();
 
   const handleDeleteButtonClick = (): void => {
@@ -20,18 +29,49 @@ const TodoItem: React.FunctionComponent<IProps> = ({ todo }) => {
     dispatch(toggleTodoReady({ id: todo.id }));
   };
 
+  const handleSaveButtonClick = (): void => {
+    if (newText !== todo.text) {
+      dispatch(changeTextTodo({ id: todo.id, text: newText }));
+
+      sendTodoUpdates({ todo: { ...todo, text: newText } });
+    }
+
+    setIsEditMode(false);
+  };
+
   return (
     <div className="todoItem">
-      <div className="todoTitle">{todo.title}</div>
-      <div className={todo.isReady ? "readyTextBlock" : "textBlock"}>
-        {todo.text}
+      <div className="todoItemBlock">
+        <div className="todoTitle">{todo.title}</div>
+        {isEditMode ? (
+          <div>
+            <input
+              className="changeTextBlock"
+              onChange={(text) => setNewText(text.target.value)}
+              value={newText}
+              placeholder="Enter text"
+            />
+            <button onClick={handleSaveButtonClick}>SAVE</button>
+          </div>
+        ) : (
+          <div className={todo.isReady ? "readyTextBlock" : "textBlock"}>
+            {todo.text}
+          </div>
+        )}
       </div>
+
       <div className="todoItemButtons">
         <div className="todoItemButton" onClick={handleReadyTodoClick}>
           {todo.isReady ? icons.readyCheckMark : icons.checkMark}
         </div>
         <div className="todoItemButton" onClick={handleDeleteButtonClick}>
           {icons.trash}
+        </div>
+        <div
+          className="todoItemButton"
+          onClick={() => setIsEditMode(!isEditMode)}
+        >
+          {icons.todoSettings}
         </div>
       </div>
     </div>
