522f3e126a565e96f837ad6b3e7e11477a1d1b0a
evaabsalyamova <absalyamovaeva@gmail.com>
2023-11-04 18:18:34 +0400

    Refactor app header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,9 @@
-import HeaderButtons from "./HeaderButtons";
 import "./styles.css";
 
 const Header: React.FunctionComponent = () => {
   return (
     <div className="container">
-      <span className="title">Task manager</span>
-      <HeaderButtons />
+      <span>Todo List</span>
     </div>
   );
 };
